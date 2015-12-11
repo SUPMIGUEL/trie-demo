568996e656832204fce46b9ce86b61c74c993a0c
Matt Lane <lane.matthew@gmail.com>
2015-12-11 13:39:21 -0800

    toggle form and output visibility

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,38 @@
 $(function() {
 
   var $form = $("#urlForm");
+  var $formArea = $("#form-area");
   var $trieData = $("#trie-data");
   var $responseArea = $("#response-area");
   var $autocompleteVal = $("#autocomplete-val");
   var $wordCount = $("#word-count");
   var $url = $("#url");
   var $uniqueWordCount = $("#unique-word-count");
-  var wordTrie = new Trie();
+  var $reset = $("#reset");
+  var wordTrie;
 
   $responseArea.hide();
 
   $form.on('submit', function(e) {
     e.preventDefault();
-    var formData = $(this).find('input').val();
-
-    console.log('formData outside get', formData);
+    var $urlInput = $(this).find('input');
+    var formData = $urlInput.val();
 
     $.get('/text?url='+encodeURIComponent(formData), function(res) {
-
-      console.log('formData inside get', formData);
 
       $wordCount.text(res.length);
       $uniqueWordCount.text(res.uniqueLength);
       $url.text(formData);
 
+      wordTrie = new Trie();
+
       res.data.forEach(function(word) {
         wordTrie.learn(word);
       });
       
+      $formArea.slideUp(1000, function() {
+        $urlInput.val("");
+      });
       $responseArea.fadeIn(1000);
 
     });
@@ -39,6 +43,14 @@ $(function() {
     $trieData.empty();
     wordTrie.autoComplete($this.val().trim()).forEach(function(word) {
       $trieData.append('<li>' + word + '</li>');
+    });
+  });
+
+  $reset.on('click', function() {
+    $formArea.slideDown(1000);
+    $responseArea.fadeOut(1000, function() {
+      $autocompleteVal.val("");
+      $trieData.empty();
     });
   });
 
