5731bb5c96ec63134c68423048adb912f5dc8c5e
Matt Lane <lane.matthew@gmail.com>
2015-12-11 11:03:06 -0800

    view updates

--- public/js/app.js
+++ public/js/app.js
@@ -4,15 +4,31 @@ $(function() {
   var $trieData = $("#trie-data");
   var $responseArea = $("#response-area");
   var $autocompleteVal = $("#autocomplete-val");
+  var $wordCount = $("#word-count");
+  var $url = $("#url");
+  var $uniqueWordCount = $("#unique-word-count");
+
   $responseArea.hide();
 
   $form.on('submit', function(e) {
     e.preventDefault();
     var formData = $(this).find('input').val();
 
+    console.log('formData outside get', formData);
+
     $.get('/text?url='+encodeURIComponent(formData), function(res) {
 
-      $trieData.append(res.data);
+      console.log('formData inside get', formData);
+
+      $wordCount.text(res.length);
+      $uniqueWordCount.text(res.uniqueLength);
+      debugger;
+      $url.text(formData);
+
+      res.data.forEach(function(word) {
+        $trieData.append('<li>' + word + '</li>');
+      });
+      
       $responseArea.fadeIn(1000);
 
     });
